c6d3def7023d362c57977341886446f9397f04a4
sein0211 <sein4037@gmail.com>
2023-06-30 23:34:25 +0900

    [feat] 날씨 도움말 페이지 내용 수정

--- app/src/main/java/com/starrynight/tourapiproject/weatherPage2/WeatherHelpActivity.java
+++ app/src/main/java/com/starrynight/tourapiproject/weatherPage2/WeatherHelpActivity.java
@@ -11,17 +11,23 @@ import com.starrynight.tourapiproject.R;
 
 public class WeatherHelpActivity extends AppCompatActivity {
 
-    private ImageView help_1_button; // 별 쉽게 관찰하기
-    private ImageView help_2_button; // 별 사진 제대로 찍기
-    private ImageView help_3_button; // 별 관측 준비물
-    private ImageView help_4_button; // 별 관측 에티켓
-    private ImageView help_5_button; // 은하수를 보는 법
+    private ImageView help_1_button;
+    private ImageView help_2_button;
+    private ImageView help_3_button;
+    private ImageView help_4_button;
+    private ImageView help_5_button;
+    private ImageView help_6_button;
+    private ImageView help_7_button;
+    private ImageView help_8_button;
 
     private TextView help_1_text;
     private TextView help_2_text;
     private TextView help_3_text;
     private TextView help_4_text;
     private TextView help_5_text;
+    private TextView help_6_text;
+    private TextView help_7_text;
+    private TextView help_8_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,12 +39,18 @@ public class WeatherHelpActivity extends AppCompatActivity {
         help_3_button = findViewById(R.id.help_3_button);
         help_4_button = findViewById(R.id.help_4_button);
         help_5_button = findViewById(R.id.help_5_button);
+        help_6_button = findViewById(R.id.help_6_button);
+        help_7_button = findViewById(R.id.help_7_button);
+        help_8_button = findViewById(R.id.help_8_button);
 
         help_1_text = findViewById(R.id.help_1_text);
         help_2_text = findViewById(R.id.help_2_text);
         help_3_text = findViewById(R.id.help_3_text);
         help_4_text = findViewById(R.id.help_4_text);
         help_5_text = findViewById(R.id.help_5_text);
+        help_6_text = findViewById(R.id.help_6_text);
+        help_7_text = findViewById(R.id.help_7_text);
+        help_8_text = findViewById(R.id.help_8_text);
 
         help_1_button.setOnClickListener(view -> {
             if (help_1_text.getVisibility() == View.GONE) {
@@ -87,6 +99,36 @@ public class WeatherHelpActivity extends AppCompatActivity {
             } else {
                 help_5_button.setRotation(0);
                 help_5_text.setVisibility(View.GONE);
+            }
+        });
+
+        help_6_button.setOnClickListener(view -> {
+            if (help_6_text.getVisibility() == View.GONE) {
+                help_6_button.setRotation(180);
+                help_6_text.setVisibility(View.VISIBLE);
+            } else {
+                help_6_button.setRotation(0);
+                help_6_text.setVisibility(View.GONE);
+            }
+        });
+
+        help_7_button.setOnClickListener(view -> {
+            if (help_7_text.getVisibility() == View.GONE) {
+                help_7_button.setRotation(180);
+                help_7_text.setVisibility(View.VISIBLE);
+            } else {
+                help_7_button.setRotation(0);
+                help_7_text.setVisibility(View.GONE);
+            }
+        });
+
+        help_8_button.setOnClickListener(view -> {
+            if (help_8_text.getVisibility() == View.GONE) {
+                help_8_button.setRotation(180);
+                help_8_text.setVisibility(View.VISIBLE);
+            } else {
+                help_8_button.setRotation(0);
+                help_8_text.setVisibility(View.GONE);
             }
         });
 
