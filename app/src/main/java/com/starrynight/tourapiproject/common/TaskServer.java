0493d9dab0693a38eda99b0730aef958c6a0c931
gyulchyoung <hacha2011@naver.com>
2024-01-02 22:51:00 +0900

    [FIX] gradle파일 수정

--- app/src/main/java/com/starrynight/tourapiproject/common/TaskServer.java
+++ app/src/main/java/com/starrynight/tourapiproject/common/TaskServer.java
@@ -15,9 +15,8 @@ import com.starrynight.tourapiproject.BuildConfig;
  */
 public class TaskServer {
 
-        public static final String serverURL = "http://192.168.45.69:8080/v2/";
+        public static final String serverURL = BuildConfig.SPRING_SERVER_V2;
 //        public static final String v1Server = BuildConfig.SPRING_SERVER_V1;
-//    public static final String v1Server = "http://192.168.45.69:8080/v2/";
 
     public static final String kkoMapURL = "https://dapi.kakao.com/";
 
